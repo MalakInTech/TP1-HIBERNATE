030e7c81e2d49229f4d7e9e8d06bfb553402a5bc
MalakInTech <el.malak.etu@gmail.com>
2026-02-17 23:03:27 +0100

    tpJPA

---
+++ src/main/java/com/example/model/Produit.java
@@ -0,0 +1,57 @@
+package com.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Produit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private BigDecimal prix;
+
+    // Constructeur obligatoire pour JPA
+    public Produit() {
+    }
+
+    public Produit(String nom, BigDecimal prix) {
+        this.nom = nom;
+        this.prix = prix;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public BigDecimal getPrix() {
+        return prix;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrix(BigDecimal prix) {
+        this.prix = prix;
+    }
+
+    @Override
+    public String toString() {
+        return "Produit{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prix=" + prix +
+                '}';
+    }
+}
+
